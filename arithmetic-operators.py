2a0e1494f1f38b49404e1509ebead2b94e980dfa
Biniyam Getachew <binigetachew18@gmail.com>
2025-04-09 21:05:04 +0300

    Solve Arthimetic operation

---
+++ arithmetic-operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    add = a + b
+    substract = a-b
+    multiply = a*b
+    print(add)
+    print(substract)
+    print(multiply)
+    
+    
